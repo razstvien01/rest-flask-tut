4e3bfb6be338448d5cbea690207ec5331610a254
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-20 12:19:58 +0800

    added Video and Helloworld class controller

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 
 api = Api(app)
 
+names = {
+  "tim": {
+    "age": 19,
+    "gender": "male"
+  },
+  "bill": {
+    "age": 70,
+    "gender": "male"
+  }
+}
+
 class HelloWorld(Resource):
-  def get(self, name, test):
-    return {"name": name, "test": test}
+  def get(self, name):
+    return names[name]
   
   # def post(self):
   #   return {"data": "Posted"}
+  
+videos = {}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+class Video(Resource):
+  def get(self, video_id):
+    return videos[video_id]
+  
+  def put(self, video_id):
+    args = video_put_args.parse_args()
+    print(request.form['likes'])
+    
+    videos[video_id] = args
+    # return {video_id: args}
+    return videos[video_id], 201  # 201 - created, 200 - okay
+  
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
   app.run(debug=True)
--- test.py
+++ test.py
@@ -2,6 +2,13 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")\
-# response = requests.post(BASE + "helloworld")
+# response = requests.get(BASE + "helloworld")\
+# # response = requests.post(BASE + "helloworld")
+# print(response.json())
+
+response = requests.put(BASE + "video/1", {"likes": 10, "name": "Nico", "views": 100000})
+print(response.json())
+
+input()
+response = requests.get(BASE + "video/1")
 print(response.json())
